6c0949b7c46878122200fe4d740e7551ccd49941
ElsdonC <elsdonc21@gmail.com>
2023-05-18 18:20:25 -0700

    patched security vulnerability in uploading image files

--- public/js/create.js
+++ public/js/create.js
@@ -41,4 +41,64 @@ function validateForm() {
         locationField.focus();
         return false;
     }
+    if (imgFileName == "") {
+        let imageField = document.getElementById("image");
+        let imageError = document.createElement("div");
+        imageError.className = "error-message";
+        imageError.textContent = "Please upload an image";
+        imageField.insertAdjacentElement("afterend", imageError);
+        imageField.focus();
+        return false;
+    }
+}
+
+let imgFileName = ""
+let imgInput = document.getElementById('image');
+let allowedExtensions = [".apng", ".avif", ".jpeg", ".jpg", ".png", ".svg+xml", ".webp"];
+
+imgInput.addEventListener("change", function() {
+    let file = this.files[0];
+
+    let fileReader = new FileReader();
+    fileReader.onloadend = function() {
+        let arrayBuffer = new Uint8Array(fileReader.result).subarray(0, 4);
+        let header = '';
+        for (let i = 0; i < arrayBuffer.length; i++) {
+            header += arrayBuffer[i].toString(16);
+        }
+        let fileType = getFileType(header);
+
+        if (fileType && allowedExtensions.includes(fileType)) {
+            imgFileName = file.name;
+        } else {
+            imgInput.value = '';
+            let imageField = document.getElementById("image");
+            let imageError = document.createElement("div");
+            imageError.className = "error-message";
+            imageError.textContent = "Unsupported Image Uploaded, try again";
+            imageField.insertAdjacentElement("afterend", imageError);
+            imageField.focus();
+        }
+    };
+    fileReader.readAsArrayBuffer(file);
+});
+
+function getFileType(header) {
+    switch (header) {
+        case "89504e47":
+            return ".png";
+        case "ffd8ff":
+            return ".jpeg";
+        case "47494638":
+            return ".gif";
+        case "25504446":
+            return ".pdf";
+        case "49492a00":
+        case "4d4d002a":
+            return ".tiff";
+        case "52494646":
+            return ".webp";
+        default:
+            return null;
+    }
 }
