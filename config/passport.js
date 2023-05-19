8e5dbe6f8b5e44d4f0f8ad1c4cd1abf42117038f
ElsdonC <elsdonc21@gmail.com>
2023-05-18 21:56:37 -0700

    installed dotenv

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID:     process.env.GOOGLE_CLIENT_ID,
+    clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback",
   },
